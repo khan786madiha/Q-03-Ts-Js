0df35685a073b572fcda8d0e8ad570188afe3d0f
Madiha Khan <159880897+khan786madiha@users.noreply.github.com>
2024-02-23 03:14:16 -0800

    Q-03 Ts/Js & node.js of 45 assignments.
    Madiha Khan's NODEJS 45 Assignment Q-03

---
+++ Q3.js
@@ -0,0 +1,9 @@
+"use strict";
+// 3.	Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+let personName = "Madiha Khan";
+// Print lowercase
+console.log("Lowercase: " + personName.toLowerCase());
+// Print uppercase
+console.log("Uppercase: " + personName.toUpperCase());
+// Print titlecase 
+console.log("Titlecase: " + personName.charAt(0).toUpperCase() + personName.slice(1));
---
+++ Q3.ts
@@ -0,0 +1,16 @@
+// 3.	Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+
+let personName: string = "Madiha Khan";
+
+// Print lowercase
+console.log("Lowercase: " + personName.toLowerCase());
+
+// Print uppercase
+console.log("Uppercase: " + personName.toUpperCase());
+
+// Print titlecase 
+console.log("Titlecase: " + personName.charAt(0).toUpperCase() + personName.slice(1));
+
+
+
+
